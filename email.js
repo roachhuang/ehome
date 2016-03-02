cf21ea015a9ca56dc575f5eab2cbb4bb04eb542e
Mark Huang <giraftw2002@gmail.com>
2016-03-02 17:20:40 +0800

    1st commit

---
+++ email.js
@@ -0,0 +1,30 @@
+var nodemailer = require('nodemailer');
+module.exports = function() {
+    var smtpConfig = {
+        service: 'gmail',
+        auth: {
+            user: 'giraftw2002@gmail.com',
+            pass: '0916@tpe'
+        }
+    };
+
+    // create reusable transporter object using the default SMTP transport 
+    var transporter = nodemailer.createTransport(smtpConfig);
+
+    // setup e-mail data with unicode symbols 
+    var mailOptions = {
+        from: 'smart home <giraftw2002@gmail.com>', // sender address 
+        to: 'mark.huang@ca-sec.com', // list of receivers 
+        subject: 'Intruder', // Subject line 
+        text: 'Hello world', // plaintext body 
+        html: '<b>Hello world</b>' // html body 
+    };
+
+    // send mail with defined transport object 
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+};
---
+++ public/myapp.js
@@ -0,0 +1,57 @@
+angular.module("myApp", ['ngSanitize', 'ngRoute'])
+
+.controller("mainCtrl", function($scope, $http, $route, $routeParams, $location) {  
+	$scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+    $scope.yahoo = {};  
+    $http.get('//localhost:3000/api/yahoo').then(function(res) {
+    	$scope.yahoo = res.data.query.results.channel;
+    });
+})
+
+.controller("nasaCtrl", function($scope, $http) {
+    $scope.apod = {};   
+    $http.get('//localhost:3000/api/apod').then(function(res) {
+    	$scope.apod = res.data;
+    });
+})  
+
+.controller("cameraCtrl", function($scope, $http, $routeParams) {
+	$scope.params = $routeParams;	// params can be camerid or eles.
+    $scope.camera = {};   
+    $http.get('http://192.168.1.252/image.jpg').then(function(res) {
+       	$scope.camera.img = res.data;
+    });
+    $http.get('http://192.168.1.252/video.cgi').then(function(res) {
+       	$scope.camera.cgi = res.data;
+    });
+})
+
+.config(function($routeProvider, $locationProvider, $httpProvider) {
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  	$routeProvider
+   	.when('/cameraView', {
+    	templateUrl: './views/cameraview.html',
+    	controller: 'cameraCtrl'
+    	/*
+    	controller: 'cameraCtrl',
+    	resolve: {
+      		// I will cause a 1 second delay
+      		delay: function($q, $timeout) {
+        		var delay = $q.defer();
+        		$timeout(delay.resolve, 1000);
+        		return delay.promise;
+      		}
+    	}
+    	*/
+	})
+  	.when('/nasa', {
+    	templateUrl: './views/nasa.html',
+    	controller: 'nasaCtrl'
+  	})
+  	.otherwise({
+  		controller: 'nasaCtrl',
+  		templateUrl: './views/nasa.html'
+  	});
+});
