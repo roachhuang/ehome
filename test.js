14a36de2beacee0dc56d6a6cdcfacd2b9a2eb148
Mark Huang <giraftw2002@gmail.com>
2016-03-30 14:05:18 +0800

    css for widgets and add bodyparser

--- test.js
+++ test.js
@@ -1,3 +1,22 @@
+
+'use strict';
+// set NODE_ENV environment variable at cmd prompt. e.g., set NODE_ENV = production 
+// port defined in server/config/env/*.js file
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development'; // has to be before config coz config reads it
+
+var express = require('express');
+var app = express();
+var config = require('./server/config/config')[env];
+
+require('./server/config/express')(app, config);
+// require(./server/config/mongoose')(config);
+
+app.use('/api', extapi);
+
+app.listen(config.port, function(req, res) {
+    console.info('Listening on port: ' +config.port + '...');
+});
 config = require("./server/config/oauth2-config.js")
 var GoogleOAuth2 = require('google-oauth2')(config);
 var scope = 'https://www.googleapis.com/auth/drive'
