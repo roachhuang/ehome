d153ca5cc955f98efbda0bd81d1255e540252dcc
Mark Huang <giraftw2002@gmail.com>
2016-05-19 17:38:50 +0800

    sensor prelimary

--- server/controllers/sensors.js
+++ server/controllers/sensors.js
@@ -1,38 +1,61 @@
 'use strict';
-
+var events = require('events');
 var email = require('../controllers/emailController');
 
-// Import events module
-var events = require('events');
-// Create an eventEmitter object
-var eventEmitter = new events.EventEmitter();
+module.exports = function () {
 
-// Bind the connection event with the listner1 function
-eventEmitter.addListener('connection', listner1);
-
-
-door.on("open", function () {
-    if (alarm.state == "on") {
-        alarm.sound();
-        hounds.release();
-    } else {
-        lights.switchOn();
-        voice.speak("Welcome home");
+    function Sensor(pin) {
+        this.pin = pin;
+        events.EventEmitter.call(this);
     }
-});
+    Sensor.prototype = new events.EventEmitter();
+    Sensor.prototype.getIoData = function (iOdata) {
+        var vm = this;
+        if (mask === D4) {
+            vm.emit('open');
+        }
+    };
 
-window.on("open", function () {
-    if (alarm.state == "on") {
-        // turn on spot light
-        // activate alarm
-        // email subject: intrudor
-        email.sendEmail;
-        // send text msg
-        // start recording video or capture video image 10 times (one time per sec)
-        // 7-eleven call police
-        // more ...
-    } else {
-        lights.switchOn();
-        voice.speak("Welcome home");
-    }
-});
+    var window = new Sensor(D4);
+    var door = new Sensor(D3);
+
+
+    door.on('open', function () {
+        if (alarm.state === 'on') {
+            alarm.sound();
+            hounds.release();
+        } else {
+            lights.switchOn();
+            voice.speak('Welcome home');
+        }
+    });
+
+    window.on('open', function () {
+        if (alarm.state === 'on') {
+            // turn on spot light
+            // activate alarm
+            // email subject: intrudor
+            email.sendEmail();
+            // send text msg
+            // start recording video or capture video image 10 times (one time per sec)
+            // 7-eleven call police
+            // more ...
+        } else {
+            lights.switchOn();
+            voice.speak('Welcome home');
+        }
+    });
+
+
+    /* Remove the binding of listner1 function
+    eventEmitter.removeListener('connection', listner1);
+    console.log("Listner1 will not listen now.");
+
+    eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+    console.log(eventListeners + " Listner(s) listening to connection event");
+    */
+     return {
+        door: door,
+        window: window
+    };
+};
