39c2e884bf89fb1a670c5bb5e7c5641c3e04963e
clsatw <mark.huang@ca-sec.com>
2016-05-20 21:33:31 +0800

    run xbee function in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require('express');
 var app = express();
 var config = require('./server/config/config')[env];
 
-var xbee = require('./server/config/xbee');
+var xbee = require('./server/config/xbee')();
 
 require('./server/config/express')(app, config);
 require('./server/config/my-passport')(app, config);
